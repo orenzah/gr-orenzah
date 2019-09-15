2c49689a54cb2c802494e53abf8991875e560172
Oren Zaharia <orenzah@gmail.com>
2019-09-15 12:04:59 +0300

    fix bound

--- python/addpreamble_bb.py
+++ python/addpreamble_bb.py
@@ -63,7 +63,11 @@ class addpreamble_bb(gr.basic_block):
 		else:
 			if (self.preamble_rem > 0):
 				for i in range(self.preamble_rem, self.preamble_len):	
-					output_items[0][i - self.preamble_rem] = 120;
+					if (i >= noutput):
+						self.preamble_rem = self.preamble_len - i;
+						return i;
+					else:	
+						output_items[0][i - self.preamble_rem] = 120;
 				self.preamble_rem = 0;				
 			else:
 				for i in range(self.preamble_len):				
