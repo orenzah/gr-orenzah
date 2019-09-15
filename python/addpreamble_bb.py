bb2d2ec10a2a5b6fd740e3d470768f09a24eacc4
Oren Zaharia <orenzah@gmail.com>
2019-09-15 15:59:58 +0300

    remove print, and increment and update npacket_out

--- python/addpreamble_bb.py
+++ python/addpreamble_bb.py
@@ -117,15 +117,14 @@ class addpreamble_bb(gr.basic_block):
 			if ((npacket_out + 1) * packet_size <= noutput):				
 				if (len(packets) == 0):
 					break;
-				packet = packets.pop(0)
-				
-				print(packet)
+				packet = packets.pop(0)						
 				for i in range(packet_size):
 					j = i + npacket_out * packet_size;
-					output_items[0][j] = packet[i];								
+					output_items[0][j] = packet[i];	
+					npacket_out += 1;							
 			else:
 				break;			
-		return (npacket_out) *(packet_size);
+		return (npacket_out) * (packet_size);
 		
 							
 			
