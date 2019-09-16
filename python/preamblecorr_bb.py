30d46f61bef688606e0bb56e6f37f0f2ef31c1bc
Oren Zaharia <orenzah@gmail.com>
2019-09-16 11:12:06 +0300

    change in sliding window

--- python/preamblecorr_bb.py
+++ python/preamblecorr_bb.py
@@ -159,6 +159,7 @@ class preamblecorr_bb(gr.basic_block):
         #return 0
         #e.g. if access_code is [1,1,3,1] and curr_window [1,2,3,4]
         #return 2
+        
         cnt = len(self.access_code_crumbs);
         for i in range(len(self.crumbs_window)):
             cnt -= (self.crumbs_window[i] == self.access_code_crumbs[i]);
